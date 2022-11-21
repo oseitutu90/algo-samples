b59260c6eee8e69d40a48dbd9d3763e1a62b7fb6
oseitutu90 <otamoabin2@icloud.com>
2022-11-21 12:30:05 -0600

    changes on tic tac toe

--- tictacToe.py
+++ tictacToe.py
@@ -1,22 +1,2 @@
-"""write a code for tictactoe """
-import random
+""""Tic tac toe code set up"""
 
-class TicTacToe:
-    
-    def __init__(self):
-        self.board = []
-        
-    def createBoard(self):
-        for i in range(3):
-            row = []
-            for j in range(3):
-                row.append("-")
-            self.board.append(row)
-            
-            
-    
-    
-
-
-tic_tic_toe = TicTacToe()
-tic_tic_toe.board()
